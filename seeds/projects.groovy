bf2af1ec270d5d41317908b0a0406508b8ec97b8
Arthur <arthurlkrieger@gmail.com>
2026-05-12 19:45:20 -0300

    Initial jenkins-infra setup with Jenkins, local registry, and seed job

---
+++ seeds/projects.groovy
@@ -0,0 +1,20 @@
+import org.yaml.snakeyaml.Yaml
+
+def config = new Yaml().load(readFileFromWorkspace('projects.yml'))
+
+config.projects.each { project ->
+    pipelineJob(project.name) {
+        description("CI pipeline for ${project.name}")
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote { url(project.repo) }
+                        branch("*/${project.branch ?: 'main'}")
+                    }
+                }
+                scriptPath('Jenkinsfile')
+            }
+        }
+    }
+}
